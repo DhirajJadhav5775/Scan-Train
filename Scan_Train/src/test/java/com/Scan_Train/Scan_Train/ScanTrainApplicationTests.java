cbd077464190b5a316e47726db105b635b9aee59
DhirajJadhav5775 <dhirajjadhav5775@gmail.com>
2025-08-07 13:48:11 +0530

    Moved backend-setup folder to root and removed backend folder

---
+++ Scan_Train/src/test/java/com/Scan_Train/Scan_Train/ScanTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scan_Train.Scan_Train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
