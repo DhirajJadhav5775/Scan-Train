748158a6024f0999200637afa1d843ed6a893daa
DhirajJadhav5775 <dhirajjadhav5775@gmail.com>
2025-09-15 18:19:24 +0530

    added signup, login and ask ai

--- Scan_Train/src/main/java/com/Scan_Train/Scan_Train/MainController/MainController.java
+++ Scan_Train/src/main/java/com/Scan_Train/Scan_Train/MainController/MainController.java
@@ -1,9 +1,8 @@
 package com.Scan_Train.Scan_Train.MainController;
 
-import com.Scan_Train.Scan_Train.DTO.AskRequest;
-import com.Scan_Train.Scan_Train.DTO.AskResponse;
-import com.Scan_Train.Scan_Train.DTO.VerifyOtpRequest;
-import com.Scan_Train.Scan_Train.DTO.userDTO;
+import com.Scan_Train.Scan_Train.DTO.*;
+import com.Scan_Train.Scan_Train.Model.User;
+import com.Scan_Train.Scan_Train.Repository.UserRepo;
 import com.Scan_Train.Scan_Train.service.GeminiService;
 import com.Scan_Train.Scan_Train.service.UserService;
 
@@ -12,6 +11,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -24,6 +25,9 @@ public class MainController
     @Autowired
     private GeminiService geminiService;
 
+    @Autowired
+    private UserRepo userRepo;
+
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody userDTO request) {
         return ResponseEntity.ok(service.signup(request));
@@ -35,10 +39,22 @@ public class MainController
         return ResponseEntity.ok(service.verifyOtp(request.getEmail(), request.getOtp()));
     }
 
-    @GetMapping("/login")
-    public void Login(@RequestParam String email, @RequestParam String password)
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request)
     {
+        Optional<User> optionalUser = userRepo.findByEmail(request.getEmail());
 
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.badRequest().body("User not found");
+        }
+
+        User user = optionalUser.get();
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            return ResponseEntity.badRequest().body("Invalid password");
+        }
+
+        return ResponseEntity.ok("Login successful! Welcome " + user.getFirstName());
     }
 
     @PostMapping("/ask")
